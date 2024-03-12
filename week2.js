bda7fb0a8ba158ac482f7e5c0e49ca21c60bf2bd
Michael-weilin <128262070+Michael-weilin@users.noreply.github.com>
2024-03-12 18:08:21 +1100

    Create week2.js
    task 1-3

---
+++ week2.js
@@ -0,0 +1,36 @@
+function task1() {
+  const length =15;
+  const width =25;
+  const area = length * width
+  console.log("the floor space is {area} square meters");
+}
+
+function task2() {
+  const minutes =197;
+  const hours = Math.floor(minutes/60);
+  const remainingMinutes = minutes % 60;
+  console.log("{minutes} minutes becomes {hours} hours and {remainingMinutes} minutes.");
+}
+
+function task3() {
+  const rates =20;
+  const loads =75;
+  const pay = rates * loads;
+  const tax = pay * 0.15;
+  const netPay = pay - tax;
+  console.log("Gross pay: {pay} Tax: {tax} Net pay: {netPay}.");
+}
+
+function task4() {
+  const length =15;
+  const width =25;
+  const area = length * width;
+  console.log("the floor space is {area} square meters");
+}
+
+function task5() {
+  const length =15;
+  const width =25;
+  const area = length * width;
+  console.log("the floor space is {area} square meters");
+}
