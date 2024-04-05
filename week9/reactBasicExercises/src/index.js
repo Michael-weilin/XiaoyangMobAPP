ba0b564f2b5731b805984606066d039185e60ad3
Michael-weilin <128262070+Michael-weilin@users.noreply.github.com>
2024-04-05 22:07:00 +1100

    TaskTwo

--- week9/reactBasicExercises/src/index.js
+++ week9/reactBasicExercises/src/index.js
@@ -2,8 +2,17 @@ import React, {StrictMode} from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
 const root = createRoot(document.getElementById('root'));
-root.render(<StrictMode><TaskOne /></StrictMode>);
+root.render(<StrictMode><TaskOne /><TaskTwoContainer /></StrictMode>);
 export function TaskOne() {
     const msg = 'Hello World'
     return(<div className='App'><h2> {msg} </h2></div>);
+};
+export function TaskTwo(props) {
+    const numbers = props.numbers;
+    const rendered = numbers.map((n) => <li key={n}>{n}</li>);
+    return rendered;
+};
+export function TaskTwoContainer() {
+    const nums = [1,2,3,4,5,6,7];
+    return(<div className='App'><ul> <TaskTwo numbers = {nums}/> </ul></div>);
 };
