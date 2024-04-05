55dfe6614ee80bde64364fa56dbc54e687dedf6c
Michael-weilin <128262070+Michael-weilin@users.noreply.github.com>
2024-04-05 22:28:53 +1100

    TaskThree

---
+++ week9/reactBasicExercises/src/data.js
@@ -0,0 +1,10 @@
+// TaskThree data.js
+class Product {
+    constructor(name, description, unitPrice) {
+        this.name = name;
+        this.description = description;
+        this.unitPrice = unitPrice;
+    }
+}
+
+export default Product;
--- week9/reactBasicExercises/src/index.js
+++ week9/reactBasicExercises/src/index.js
@@ -1,8 +1,9 @@
 import React, {StrictMode} from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
+import Product from './data';
 const root = createRoot(document.getElementById('root'));
-root.render(<StrictMode><TaskOne /><TaskTwoContainer /></StrictMode>);
+root.render(<StrictMode><TaskOne /><TaskTwoContainer /><TaskThreeContainer /></StrictMode>);
 export function TaskOne() {
     const msg = 'Hello World'
     return(<div className='App'><h2> {msg} </h2></div>);
@@ -16,3 +17,25 @@ export function TaskTwoContainer() {
     const nums = [1,2,3,4,5,6,7];
     return(<div className='App'><ul> <TaskTwo numbers = {nums}/> </ul></div>);
 };
+export function TaskThree(props) {
+    const { name, description, unit_price } = props.product;
+    return (
+        <li>
+            <strong>{name}</strong>: {description} (${unit_price.toFixed(2)})
+        </li>
+    );
+};
+function TaskThreeContainer() {
+    const productInfo = {
+        name: 'Banana bread',
+        description: 'Toasted with butter and cinnamon',
+        unit_price: 3.5,
+    };
+    return (
+        <div className='App'>
+            <ul>
+                <TaskThree product={productInfo} /> {'Laptop','High-performance laptop',1200.00}
+            </ul>
+        </div>
+    );
+}
